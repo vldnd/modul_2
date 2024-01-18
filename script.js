2d6cc0ded1d9d6a98e694b3af7a7bbf9cd377979
vldnd <vlasvdenis@yandex.ru>
2024-01-18 18:02:39 +0300

    Добавил мандаринки

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+{
+  const item = "Молоко";
+  const count = 5;
+  const category = "Молочные изделия";
+  const price = 60;
+
+  console.log(item);
+  console.log(`общая сумма товара: ${count * price}`);
+}
+
+{
+  const item = "Мандаринки";
+  const count = 20;
+  const category = "Фрукты";
+  const price = 10;
+
+  console.log(item);
+  console.log(`общая сумма товара: ${count * price}`);
+}
