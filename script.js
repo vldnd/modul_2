5649b8366aa284e0df9069b13a23529f7e2b44f0
vldnd <vlasvdenis@yandex.ru>
2024-01-19 13:52:52 +0300

    Добавил prompt

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 "use strict";
 
+// {
+//   const item = "Молоко";
+//   const count = 5;
+//   const category = "Молочные изделия";
+//   const price = 60;
+
+//   console.log(item);
+//   console.log(`общая сумма товара: ${count * price}`);
+// }
+
+// {
+//   const item = "Мандаринки";
+//   const count = 20;
+//   const category = "Фрукты";
+//   const price = 10;
+
+//   console.log(item);
+//   console.log(`общая сумма товара: ${count * price}`);
+// }
+
 {
-  const item = "Молоко";
-  const count = 5;
-  const category = "Молочные изделия";
-  const price = 60;
-
+  const item = prompt("Укажите продукт", "");
   console.log(item);
-  console.log(`общая сумма товара: ${count * price}`);
-}
+  const count = prompt("Укажите количество продукта", "");
+  console.log(count);
+  const category = prompt("Укажите категорию продукта", "");
+  console.log(category);
+  const price = prompt("Укажите цену продукта", "");
+  console.log(price);
 
-{
-  const item = "Мандаринки";
-  const count = 20;
-  const category = "Фрукты";
-  const price = 10;
-
-  console.log(item);
-  console.log(`общая сумма товара: ${count * price}`);
+  console.log(
+    `"На складе ${count} единицы товара "${item}" на сумму ${
+      count * price
+    } деревянных"`
+  );
 }
