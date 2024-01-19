6a15f05a97884c66c5f2d46a0156f2671fa6fb82
vldnd <vlasvdenis@yandex.ru>
2024-01-19 16:27:34 +0300

    Считаем зарубежные налоги

--- tasksHard_1.js
+++ tasksHard_1.js
@@ -1,15 +1,13 @@
 const selary = +prompt("Укажите размер заработной платы", "15000");
 
-switch (selary) {
-  case selary < 15000:
-    console.log(`Ваш налог составляет ${selary * 0.13}`);
-    break;
-  case selary >= 15000 && selary < 50000:
-    console.log(`Ваш налог составляет ${selary * 0.2}`);
-    break;
-  case selary >= 50000:
-    console.log(`Ваш налог составляет ${selary * 0.3}`);
-    break;
-  default:
-    console.log(`Ваш налог составляет ${selary * 0.13}`);
+if (selary < 15000) {
+  console.log(`Ваш налог составляет ${selary * 0.13}`);
+} else if (selary >= 15000 && selary < 50000) {
+  console.log(`Ваш налог составляет ${(selary - 15000) * 0.2 + 15000 * 0.13}`);
+} else {
+  console.log(
+    `Ваш налог составляет ${
+      (selary - 50000) * 0.3 + 35000 * 0.2 + 15000 * 0.13
+    }`
+  );
 }
