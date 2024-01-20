8a8487d0a850e2fa10c775df67fefdfea5e7faa3
vldnd <vlasvdenis@yandex.ru>
2024-01-20 21:18:48 +0300

    НОД

--- script.js
+++ script.js
@@ -1,46 +1,15 @@
 "use strict";
 
-// const converter = (euro) => {
-//   const dollar = euro * 1.2;
-//   const ruble = dollar * 73;
-//   return ruble;
-// };
+const divider = (a, b) => {
+  while (a != 0 && b != 0) {
+    if (a > b) {
+      a = a % b;
+    } else {
+      b = b % a;
+    }
+  }
 
-// console.log(converter(1));
+  return a + b;
+};
 
-// const getCapitalLetter = (str) => {
-//   return str[0].toUpperCase() + str.slice(1).toLowerCase();
-// };
-
-// console.log(getCapitalLetter("привет Мир!"));
-
-// const getReverseString = (str) => {
-//   return str.split("").reverse().join("");
-// };
-
-// console.log(getReverseString("Привет мир!"));
-
-// const calculate = (sum, quantity, promo) => {
-//   if (quantity > 10) {
-//     sum = sum * 0.97;
-//   }
-
-//   if (sum > 30000) {
-//     sum = sum * 0.85;
-//   }
-
-//   if (promo === "METHED") {
-//     sum = sum * 0.9;
-//   }
-
-//   if (promo === "G3H2Z1" && sum > 2000) {
-//     sum = sum - 500;
-//   }
-
-//   return sum;
-// };
-
-// console.log(calculate(20000, 11));
-// console.log(calculate(35000, 5));
-// console.log(calculate(35000, 12, "METHED"));
-// console.log(calculate(35000, 12, "G3H2Z1"));
+console.log(divider(64, 32));
