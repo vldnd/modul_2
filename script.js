9a11a50fa8276fe1b04bb13ab4b71374c6f1e65e
vldnd <vlasvdenis@yandex.ru>
2024-01-21 19:49:34 +0300

    Определитель простого числа

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 "use strict";
 
-for (let i = 2; i <= 10; i++) {
-  console.log("________________");
-  for (let j = 1; j <= 10; j++) {
-    console.log(`${i} ** ${j} = ${i ** j}`);
+const isPrime = (x) => {
+  for (let i = 2; i < x; i++) {
+    if (x % i === 0) return false;
   }
-}
+  return true;
+};
+
+// console.log(isPrime(5));
