1e4785b16ec3aacd2d5771b6e15c52d7a37baf7b
vldnd <vlasvdenis@yandex.ru>
2024-01-21 22:27:00 +0300

    Средняя стоймость товара в чеке

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 "use strict";
 
-const names = [
-  "Noah",
-  "Liam",
-  "Mason",
-  "Jacob",
-  "Robot",
-  "William",
-  "Ethan",
-  "Michael",
-  "Alexander",
+const allСashbox = [
+  [12, 4500],
+  [7, 3210],
+  [4, 650],
+  [3, 1250],
+  [9, 7830],
+  [1, 990],
+  [6, 13900],
+  [1, 370],
 ];
 
-const addPrefix = ([...arr]) => {
-  const x = "Mr ";
-
-  for (const key in arr) {
-    arr[key] = x + arr[key];
+const getAveragePriceGoods = ([...arr]) => {
+  let x = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let [key, value] = arr[i];
+    // console.log(key, value);
+    x += value / key;
+    // console.log(x);
   }
-  return arr;
+
+  return Math.floor(x / arr.length);
 };
 
-console.log(addPrefix(names));
+console.log(getAveragePriceGoods(allСashbox));
