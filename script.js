a74c17135fb7fcf6f13ab8ad1581b3da451edb1e
vldnd <vlasvdenis@yandex.ru>
2024-01-21 20:57:41 +0300

    Фильтр массив

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 "use strict";
 
-const isPrime = (x) => {
-  for (let i = 2; i < x; i++) {
-    if (x % i === 0) return false;
+const allStudents = [
+  "Иванов",
+  "Петров",
+  "Сидоров",
+  "Кузнецов",
+  "Смирнов",
+  "Попов",
+  "Соколов",
+];
+const failedStudents = ["Сидоров", "Смирнов", "Попов"];
+
+const filter = ([...arr1], [...arr2]) => {
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        arr1.splice(i, 1);
+      }
+    }
   }
-  return true;
+  return arr1;
 };
 
-// console.log(isPrime(5));
+console.log(filter(allStudents, failedStudents));
