2d4ba1cf7a13424d2e6c69f0e9f34cd5c0434b00
vldnd <vlasvdenis@yandex.ru>
2024-01-25 14:55:45 +0300

    Алгоритм вычисления високосного года

--- script.js
+++ script.js
@@ -1,23 +1,13 @@
 "use strict";
 
-const getRandomNumber = (n, m) => {
-  return Math.floor(Math.random() * (Math.abs(n - m) + 1)) + Math.min(n, m);
+const getIntercalaryYearsByRange = (n, m) => {
+  const arr = [];
+  for (let i = n; i <= m; i++) {
+    if ((i % 4 === 0 && !(i % 100 === 0)) || i % 400 === 0) {
+      arr.push(i);
+    }
+  }
+  return arr;
 };
 
-const getArrayWithRandomNumbers = (length, n, m, string) => {
-  const max = Math.max(n, m);
-  return new Array(length)
-    .fill(undefined)
-    .map(() => getRandomNumber(n, m))
-    .map((init) => {
-      if (string === "even" && init % 2 !== 0) {
-        return init === max ? init - 1 : init + 1;
-      } else if (string === "odd" && init % 2 !== 1) {
-        return init === max ? init - 1 : init + 1;
-      } else {
-        return init;
-      }
-    });
-};
-
-console.log(getArrayWithRandomNumbers(10, 5, 1, "odd"));
+console.log(getIntercalaryYearsByRange(1895, 2024));
