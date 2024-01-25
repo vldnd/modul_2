c071a9ad4e803fb4b3d70b9f3b33bc9fb02aa0a0
vldnd <vlasvdenis@yandex.ru>
2024-01-25 13:42:11 +0300

    Массив рандомных чисел

--- script.js
+++ script.js
@@ -1,26 +1,9 @@
 "use strict";
 
-const allСashbox = [
-  [12, 4500],
-  [7, 3210],
-  [4, 650],
-  [3, 1250],
-  [9, 7830],
-  [1, 990],
-  [6, 13900],
-  [1, 370],
-];
-
-const getAveragePriceGoods = ([...arr]) => {
-  let x = 0;
-  for (let i = 0; i < arr.length; i++) {
-    let [key, value] = arr[i];
-    // console.log(key, value);
-    x += value / key;
-    // console.log(x);
-  }
-
-  return Math.floor(x / arr.length);
+const getArrayWithRandomNumbers = (length) => {
+  return new Array(length)
+    .fill(undefined)
+    .map(() => Math.floor(Math.random() * 100));
 };
 
-console.log(getAveragePriceGoods(allСashbox));
+console.log(x(10));
