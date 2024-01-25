f187f3be05ab9cd45c5755756960a04590a65926
vldnd <vlasvdenis@yandex.ru>
2024-01-25 14:32:50 +0300

    Создаем массив рандомных чисел с диапазоном + задаем четность и нечетность числам

--- script.js
+++ script.js
@@ -4,8 +4,20 @@ const getRandomNumber = (n, m) => {
   return Math.floor(Math.random() * (Math.abs(n - m) + 1)) + Math.min(n, m);
 };
 
-const getArrayWithRandomNumbers = (length, n, m) => {
-  return new Array(length).fill(undefined).map(() => getRandomNumber(n, m));
+const getArrayWithRandomNumbers = (length, n, m, string) => {
+  const max = Math.max(n, m);
+  return new Array(length)
+    .fill(undefined)
+    .map(() => getRandomNumber(n, m))
+    .map((init) => {
+      if (string === "even" && init % 2 !== 0) {
+        return init === max ? init - 1 : init + 1;
+      } else if (string === "odd" && init % 2 !== 1) {
+        return init === max ? init - 1 : init + 1;
+      } else {
+        return init;
+      }
+    });
 };
 
-console.log(getArrayWithRandomNumbers(10, 5, 1));
+console.log(getArrayWithRandomNumbers(10, 5, 1, "odd"));
