db2f6e04a6a8d7c959ebbef83eda7c194d6da667
vldnd <vlasvdenis@yandex.ru>
2024-01-26 18:46:36 +0300

    Заменил цикл в игре рекурсией

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
-"use strict";
+'use strict';
 
-const cart = {
-  items: [],
-  count: 0,
-  add(product, price, quantity = 1) {
-    this.items.push({ product: product, price: price, quantity: quantity });
-  },
-  increaseCount(x) {
-    return x * this.count;
-  },
-  calculateItemPrice() {
-    return this.items.reduce(
-      (acc, curr) => acc + curr.price * curr.quantity,
-      0
-    );
-  },
-  clear() {
-    this.items = [];
-    this.totalPrice = 0;
-    this.count = 0;
-  },
-  print() {
-    const x = JSON.stringify(this.items);
-
-    console.log(x, this.totalPrice);
-  },
-  get totalPrice() {
-    return this.calculateItemPrice();
-  },
+const getGameMoreLess = (n, m) => {
+  const y = Math.floor(Math.random() * Math.abs(n - m) + 1) + Math.min(n, m);
+  console.log(y);
+  const x = +prompt(`Введите число от ${Math.min(n, m)} до ${Math.max(n, m)}`);
+  const arr = [];
+  const recursion = (x, y, arr) => {
+    if (arr.length === Math.abs(n - m) * 0.3) {
+      return 'Game over';
+    } else if (x > Math.max(n, m) || x < Math.min(n, m)) {
+      x = +prompt(`Введите число от ${Math.min(n, m)} до ${Math.max(n, m)}`);
+      return recursion(x, y, arr);
+    } else if (arr.includes(x)) {
+      x = +prompt('Вы уже вводили это число!');
+      return recursion(x, y, arr);
+    } else if (x > y) {
+      arr.push(x);
+      x = +prompt('Меньше!');
+      return recursion(x, y, arr);
+    } else if (x < y) {
+      arr.push(x);
+      x = +prompt('Больше!');
+      return recursion(x, y, arr);
+    } else {
+      return `Вы угадали, это число ${y}`;
+    }
+  };
+  const message = recursion(x, y, arr);
+  alert(message);
+  if (confirm('Хотите сыграть еще раз?')) {
+    getGameMoreLess(n, m);
+  }
 };
 
-cart.add("Молоко", 60, 3);
-cart.getTotalPrice;
-cart.print();
+console.log(getGameMoreLess(100, 50));
