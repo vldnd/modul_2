b3f44033d7626fd3ba0516fb342d61c791ef1db0
vldnd <vlasvdenis@yandex.ru>
2024-01-27 16:17:52 +0300

    Создал объект, который описывает ширину и высоту

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 'use strict';
 
-const arr = [];
-
-const newArr = arr => {
-  const x = Math.floor(Math.random() * 11);
-  arr.push(x);
-  const y = arr.reduce((acc, init) => acc + init, 0);
-  if (y < 50) {
-    return newArr(arr);
-  } else {
-    return arr;
-  }
+const rectangle = {
+  trueWidth: 0,
+  trueHeight: 0,
+  set width(w) {
+    this.trueWidth = w;
+  },
+  set height(h) {
+    this.trueHeight = h;
+  },
+  get square() {
+    return this.trueWidth * this.trueHeight;
+  },
+  get perimetr() {
+    return this.trueWidth + this.trueHeight;
+  },
 };
 
-console.log(newArr(arr));
+rectangle.width = 10;
+rectangle.height = 10;
+
+console.log(rectangle.square);
+console.log(rectangle.perimetr);
