6a18dc626b6ecc765d7dad0e00d5b21d3e0060b6
vldnd <vlasvdenis@yandex.ru>
2024-02-09 17:15:06 +0300

    cms

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cms-layout
+
+git checkout -b ''
---
+++ js/script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const cmsTitle = document.querySelector('.cms__title');
+const modalForm = document.querySelector('.modal__form');
+const modalCheckbox = document.querySelector('.modal__checkbox');
+const modalInputDiscount = document.querySelector('.modal__input_discount');
